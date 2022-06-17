f8f89f808b94a149a7fe82a47ef79f8b6bf54173
Guilherme <gui.lima2304@gmail.com>
2022-06-17 01:49:45 -0300

    structure created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { MainComponent } from './components/main.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { CharactersComponent } from './components/menu/characters/characters.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    NavbarComponent,
+    MenuComponent,
+    CharactersComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, MainComponent, NavbarComponent]
 })
 export class AppModule { }
